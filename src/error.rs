84bd3bbe9245c8d2c5313eb41d9327f3d78506d3
Barret Rennie <barret@brennie.ca>
2018-09-23 15:33:47 -0400

    cargo fmt

--- src/error.rs
+++ src/error.rs
@@ -15,8 +15,10 @@ pub enum Error {
     /// An error that occurs when a sequence of bytes does not correctly parse as a wake-on-LAN magic packet.
     MagicPacketParseError(#[cause] easy::Errors<u8, String, usize>),
 
-    #[fail(display = "Invalid packet length ({}); wake-on-LAN magic packets should be 106 bytes",
-           _0)]
+    #[fail(
+        display = "Invalid packet length ({}); wake-on-LAN magic packets should be 106 bytes",
+        _0
+    )]
     /// An error that occurs when a sequence of bytes is the wrong length to be a wake-on-LAN magic packet.
     MagicPacketLengthError(usize),
 
